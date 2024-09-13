666dfb5c39b297aaff0a8a57a074887e34b52f4f
neosharma995 <mytravplan@gmail.com>
2024-09-13 17:15:42 +0530

    Add untracked files

--- src/app/_seo_metadata/metadata.jsx
+++ src/app/_seo_metadata/metadata.jsx
@@ -3,7 +3,7 @@ export async function LoadscoData({data}) {
     const metadata = {
         title: data.sco_title || 'no title',
         description: data.sco_description || 'no description' ,
-        description: data.sco_description || 'no description' 
+        description: data.sco_description || 'no description', 
         description: data.sco_description || 'no description' 
     };
     return metadata;
