57369953ada71bb9eb400e6e2e53ae78d9ac974a
neosharma995 <neosharma995@gmail.com>
2024-11-05 17:31:20 +0530

    modified all pages

--- src/app/(blog)/Components/blogpage.jsx
+++ src/app/(blog)/Components/blogpage.jsx
@@ -1,35 +1,28 @@
-'use client'
-import React, { useEffect, useMemo, useState } from 'react';
+'use client';
 
-
+import React, { useState } from 'react';
 import { PER_PAGE_LIMIT } from '@/utils/apis/api';
-
 import BlogCard from './blogCard';
 import useFetchAllSections from '@/hooks/useLoadApiHook';
- 
-const BlogCardsContainer = () => {
 
-const [page, setPage] = useState(1);
+const BlogCardsContainer = () => {
+  const [page, setPage] = useState(1);
 
   const response = useFetchAllSections(page, PER_PAGE_LIMIT);
   const { blogs = [], pagination = {} } = response.data || {};
   const { totalBlogs = 0 } = pagination;
 
-  const memoizedBlogs = useMemo(() => ({
-    blogs: blogs,
-  }), [ blogs]);
+  // Directly reverse the blogs array without useMemo
+  const reversedBlogs = Array.isArray(blogs) ? [...blogs].reverse() : [];
 
-  
-
-  let reversedBlogs=Array.isArray(memoizedBlogs.blogs)?[...memoizedBlogs.blogs].reverse():[]
   return (
     <>
       <BlogCard 
-      result={reversedBlogs} 
-      setPage={setPage}
-      page={page}
-      totalBlogs={totalBlogs}
-      limit={PER_PAGE_LIMIT}
+        result={reversedBlogs} 
+        setPage={setPage}
+        page={page}
+        totalBlogs={totalBlogs}
+        limit={PER_PAGE_LIMIT}
       />
     </>
   );
