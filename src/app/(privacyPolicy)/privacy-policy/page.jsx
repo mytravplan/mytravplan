8a41d131c88a5ef92dd03cdd6e4d67efa302633d
neosharma995 <neosharma995@gmail.com>
2024-11-07 17:47:08 +0530

    updated code

--- src/app/(privacyPolicy)/privacy-policy/page.jsx
+++ src/app/(privacyPolicy)/privacy-policy/page.jsx
@@ -3,29 +3,30 @@ import { EXPORT_ALL_APIS } from '@/utils/apis/api';
 import React from 'react'
 
 export default async function page() {
-  let api=EXPORT_ALL_APIS()
-  let resp=await api.fetchPrivacyPolicy()
-  let data=resp.result[0]||[]
+  // Fetching privacy policy data
+  let api = EXPORT_ALL_APIS();
+  let resp = await api.fetchPrivacyPolicy();
+
+  
+  let data = Array.isArray(resp?.result) && resp?.result.length > 0 ? resp.result[0] : [];
+
   return (
     <>
-    <Layout>
-      <div className="privacy_outer">
-        <div className="privacy_inner">
-
-      {data?.privacydata===undefined?'no result found':data?.privacydata?.map((ele,index)=>{
-        return  <div className="privacy-policy-container"  key={index}>
-          <h2>{ele?.title}</h2>
-          <p>{ele?.description}</p>
-
+      <Layout>
+        <div className="privacy_outer">
+          <div className="privacy_inner">
+            
+            {data?.privacydata === undefined ? 'No result found' : (
+              data?.privacydata?.map((ele, index) => (
+                <div className="privacy-policy-container" key={index}>
+                  <h2>{ele?.title}</h2>
+                  <p>{ele?.description}</p>
+                </div>
+              ))
+            )}
+          </div>
         </div>
-      })}
-        </div>
-
-      </div>
-    
-    </Layout>
+      </Layout>
     </>
-  )
+  );
 }
-
- 
