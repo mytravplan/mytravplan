d2eb7998b01b5a586eaefaaeef832b4362561c42
moolraaj <116911577+moolraaj@users.noreply.github.com>
2025-05-30 17:38:09 +0530

    once modify the next config file

--- next.config.mjs
+++ next.config.mjs
@@ -6,42 +6,19 @@ const nextConfig = {
             {
                 source: "/api/:path*",
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { 
-                        key: "Access-Control-Allow-Origin", 
-                        value: "https://auth.otpless.app"  
-                    },
-                    { 
-                        key: "Access-Control-Allow-Origin", 
-                        value: "https://marketing.otpless.app"  
-                    },
-                    { 
-                        key: "Access-Control-Allow-Origin", 
-                        value: "https://mytravplan.com"  
-                    },
-                    { 
-                        key: "Access-Control-Allow-Origin", 
-                        value: "http://localhost:3000"  
-                    },
-                    { 
-                        key: "Access-Control-Allow-Origin", 
-                        value: "https://www.mytravplan.com"  
-                    },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { 
-                        key: "Access-Control-Allow-Headers", 
-                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
-                    },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ],
             },
         ];
     },
-    
+
     async rewrites() {
         return [
             {
                 source: "/uploads/:path*",
-                destination: "/uploads/:path*",  
+                destination: "/uploads/:path*",
             },
         ];
     },
